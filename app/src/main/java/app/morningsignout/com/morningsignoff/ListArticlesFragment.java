20a58b080c1e76aa0dfc4edd68efb0287a3f8de3
Fire3galaxy <d.handojo2013@live.com>
2015-04-01 10:57:43 -0700

    Revert "Basic Setup Done!"
    This reverts commit a19a7ca3ccc7debb7bed68347675b5b80579e49e.

---
+++ app/src/main/java/app/morningsignout/com/morningsignoff/ListArticlesFragment.java
@@ -0,0 +1,10 @@
+package app.morningsignout.com.morningsignoff;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Daniel on 3/25/2015.
+ */
+public class ListArticlesFragment extends Fragment {
+
+}
