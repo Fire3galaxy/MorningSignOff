65172917dc3f96ea2ffdf2f41229f485efa9f27f
Fire3galaxy <d.handojo2013@live.com>
2015-04-02 16:59:42 -0700

    HEADLINE fragment can display multiple pictures

--- app/src/main/java/app/morningsignout/com/morningsignoff/HeadlineFragment.java
+++ app/src/main/java/app/morningsignout/com/morningsignoff/HeadlineFragment.java
@@ -1,7 +1,7 @@
 package app.morningsignout.com.morningsignoff;
 
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,7 +13,25 @@ import android.widget.ImageButton;
  * Created by Daniel on 3/2/2015.
  */
 public class HeadlineFragment extends Fragment {
+    final static String IMAGE_NUMBER = "image number";
+    private int page_number;
+
     public HeadlineFragment() {
+    }
+
+    // A "Factory" method that creates different images when scrolled through
+    public static HeadlineFragment create(int pageNumber) {
+        HeadlineFragment newImage = new HeadlineFragment();
+        Bundle args = new Bundle();
+        args.putInt(IMAGE_NUMBER, pageNumber);
+        newImage.setArguments(args);
+        return newImage;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page_number = getArguments().getInt(IMAGE_NUMBER);
     }
 
     @Override
@@ -22,10 +40,30 @@ public class HeadlineFragment extends Fragment {
         final String HEADLINE = "HEADLINE"; // for log cat
 
         View rootView = inflater.inflate(R.layout.fragment_headline, container, false);
+        ImageButton ib = (ImageButton) rootView.findViewById(R.id.imageButton_headline);
 
-        ImageButton imgButton_frag = (ImageButton) rootView.findViewById(R.id.container_headline);
-        Bitmap head = BitmapFactory.decodeResource(getResources(), R.drawable.gravityfallsteaser);
-        imgButton_frag.setImageBitmap(head);
+        if (page_number == 0)
+            ib.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.gravityfallsteaser));
+        else if (page_number == 1)
+            ib.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.the_flash));
+        else if (page_number == 2)
+            ib.setImageResource(R.drawable.steven_universe_by_flafly_d6zv94s);
+
+
+// When I thought this was a sideways list (it's a series of pages...)
+//        ArrayList<Bitmap> heads = new ArrayList<Bitmap>();
+//        heads.add(BitmapFactory.decodeResource(getResources(), R.drawable.gravityfallsteaser));
+//        heads.add(BitmapFactory.decodeResource(getResources(), R.drawable.heartrate));
+//        heads.add(BitmapFactory.decodeResource(getResources(), R.drawable.the_flash));
+//
+//        final ArrayAdapter<Bitmap> bitmapAdapter = new ArrayAdapter<Bitmap>(
+//                getActivity(),
+//                R.layout.list_items_headline,
+//                R.id.container_headline,
+//                heads
+//        );
+//
+//        ListView lv_frag = (ListView) rootView.findViewById(R.id.listview_headline);
 
         return rootView;
     }
--- app/src/main/java/app/morningsignout/com/morningsignoff/MainActivity.java
+++ app/src/main/java/app/morningsignout/com/morningsignoff/MainActivity.java
@@ -1,5 +1,25 @@
+/*
+ * Copyright 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package app.morningsignout.com.morningsignoff;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -20,23 +40,54 @@ import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+    // ----------------------------------For Headline Paging Capability
+    private static final int NUM_PAGES = 3; // FIXME: INTENDED TO BE 5
+    private ViewPager headlinePager;
+    private PagerAdapter headlinePagerAdapter;
+
+    /**
+     * A simple pager adapter that represents 5 HeadlineFragment objects, in
+     * sequence. Copied from http://developer.android.com/training/animation/screen-slide.html
+     * Each image button will be a page instantiated by the pager adapter
+     */
+    private class HeadlinePagerAdapter extends FragmentStatePagerAdapter {
+        public HeadlinePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return HeadlineFragment.create(position);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+    // ------------------------------------------For Headline Paging Capability
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        super.getSupportActionBar().setDisplayHomeAsUpEnabled(true); //made back arrow in top left corner
+
+        // The pager for list of headline images (each is own page)
+        headlinePager = (ViewPager) findViewById(R.id.container_headline);
+        headlinePagerAdapter = new HeadlinePagerAdapter(getSupportFragmentManager());
+        headlinePager.setAdapter(headlinePagerAdapter);
+
+        // Bottom half of screen: The list of category buttons
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new MainPageFragment())
                     .commit();
-
-        }
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container_headline, new HeadlineFragment())
-                    .commit();
-        }
+        } // Top half of screen: The list of latest article images
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container_headline, new HeadlineFragment())
+//                    .commit();
+//        }
     }
 
 
@@ -61,6 +112,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
 }
